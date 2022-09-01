8536fd56a8e7cd21997021ed7f3252bb6f90097e
roseoutz <roseoutz@gmail.com>
2022-09-01 13:59:45 +0900

    be-my-friend init

---
+++ src/main/java/me/friend/BeMyFriendApplication.java
@@ -0,0 +1,13 @@
+package me.friend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeMyFriendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BeMyFriendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/me/friend/BeMyFriendApplicationTests.java
@@ -0,0 +1,13 @@
+package me.friend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeMyFriendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
